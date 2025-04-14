1feec99b5082f9bea1500351a77242115e5b43f5
Steven Cady <stcady054@gmail.com>
2025-04-14 01:39:16 -0700

    functions

--- programming/introduction-to-programming/python-1/turtle1.py
+++ programming/introduction-to-programming/python-1/turtle1.py
@@ -6,3 +6,18 @@ fred.right(135)
 fred.forward(140)
 fred.right(135)
 fred.forward(100)
+
+def draw_square():
+    for i in range(4):
+        jack.forward(100)
+        jack.right(90)
+
+for square in range(10):
+    jack = turtle.Turtle()
+    jack.color("red")
+    jack.speed(0)
+    draw_square()
+    jack.penup()
+    jack.forward(5)
+    jack.left(5)
+    jack.pendown()
