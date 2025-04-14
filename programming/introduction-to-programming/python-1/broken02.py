66e185fc268570016d6c84cfba804f31a2cd120d
Steven Cady <stcady054@gmail.com>
2025-04-14 15:24:40 -0700

    python at home

---
+++ programming/introduction-to-programming/python-1/broken02.py
@@ -0,0 +1,10 @@
+import random
+
+def coin_flip():
+	return random.choice(['heads', 'tails'])
+
+if coin_flip() == 'heads':
+	print('Heads - You win!')
+else:
+	print('Tails - You lose!')
+
---
+++ programming/introduction-to-programming/python-1/function_review.py
@@ -0,0 +1,15 @@
+def say_hello():
+    return "Hello!"
+
+print(say_hello())
+
+def confuse():
+    print("bears")
+    return 42
+
+confuse()
+
+def more_confused():
+    2 + 2
+
+print(more_confused())
