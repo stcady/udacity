44185e0a05b6c26a8baceac511d0c8b1e383a86f
Steven Cady <stcady054@gmail.com>
2025-04-15 00:18:44 -0700

    list methods

---
+++ programming/introduction-to-programming/python-1/lists.py
@@ -0,0 +1,12 @@
+# Enter python3 in the terminal below to start interactive mode. Then, create this list:
+words = ["echidna", "dingo", "crocodile", "bunyip"]
+words.append("platypus")
+print(words)
+words.extend("abc")
+print(words)
+words.extend(["kangaroo", "wallaby"])
+print(words)
+words.reverse()
+print(words)
+words.sort()
+print(words)
